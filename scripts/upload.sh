8758edf76bcbea0ce9b165cbb64ae2181df9747f
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2022-02-20 21:23:46 +0000

    Correct file paths

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -13,11 +13,11 @@ echo ls -la
 
 echo "Uploading: <${PAGE_DIRECTORY}> (from <${BRANCH}> @ <${COMMIT_SHA}>) to <${S3_BUCKET}>"
 
-for entry in "$PAGE_DIRECTORY"/*; do
+for entry in "${PAGE_DIRECTORY}"/*; do
     item=$(echo "${entry}" | sed 's/.*\///')  # getting the name of the file or directory
     if [[ -d  ${entry} ]]; then  # if it is a directory
-        aws s3 cp "${item}" "s3://${S3_BUCKET}/${item}/" --recursive --region eu-west-1
+        aws s3 cp "./${item}" "s3://${S3_BUCKET}/${item}/" --recursive --region eu-west-1
     else  # if it is a file
-        aws s3 cp "${item}" "s3://${S3_BUCKET}/" --region eu-west-1
+        aws s3 cp "./${item}" "s3://${S3_BUCKET}/" --region eu-west-1
     fi
 done
